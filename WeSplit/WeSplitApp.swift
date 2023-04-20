b34bd421931e852b7a00a284956214b146285eb4
Berardino Chiarello <berardino95@gmail.com>
2023-04-20 19:45:56 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by CHIARELLO Berardino - ADECCO on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
