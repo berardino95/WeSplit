1800852a47917a8686574ab17918430e2375e4af
Berardino Chiarello <berardino95@gmail.com>
2023-04-21 10:11:17 +0200

    All the functionality implemented + Readme info added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WeSplit
+
 App that allows you to split the check
+
+Entering the check amount, and selecting the number of people and how much tips do you want to leave, the app calculate the total per person.
+
+-- Tips in the app
+
+Used @FocusState to hide the keyboard with a button on the ToolbarItemGroup in the keyboard
+
+Textfield with format option to show the correct type of value, es .percentage or .currency
+
+The app
+Read currency local data from Locale.current-currency.identifier
+
+------
--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    //A particular wrapper to handle keyboard state
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10 , 15 , 20 , 25 , 30]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView{
             Form{
@@ -18,18 +33,43 @@ struct ContentView: View {
                     TextField("Amount", value: $checkAmount, format:
                             .currency(code: Locale.current.currency?.identifier ?? "EUR"))
                     .keyboardType(.decimalPad)
+                    //change the state of the variable if the Textfield is selected
+                    .focused($amountIsFocused)
+                    
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<10) {
                             Text("\($0) people")
                         }
                     }
                 }
+                    
+                    Section{
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        Text("How much tip do you want to leave?")
+                    }
+                
+                 
                 Section  {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR" ))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR" ))
                 }
             }
             .navigationTitle("WeSplit")
-            .navigationBarTitleDisplayMode(.inline)
+            //Add a toolbar to the keyboard with a button that trigger amountIsFocused and hide the keyboard
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.large)
         }
     }
 }
